69fc144d7297525647440afd59bfabc130558fad
alirafique99 <mali@icaremanager.com>
2019-04-04 14:43:38 +0500

    initial

---
+++ testnetcore/models/Values.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testnetcore.models
+{
+    public class Values
+    {
+        private string [] values = new string[] { "value1", "value2" };
+        public string[] getValues() {
+            return values;
+        }
+
+        //public string[] getValue(int id)
+        //{
+        //    return values[id];
+        //}
+
+    }
+}
